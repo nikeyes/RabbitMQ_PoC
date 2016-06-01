f233576ec65ea1878eb5965b82fd2cae37e14152
Jorge Castro <nikey_es@yahoo.es>
2016-06-02 00:19:28 +0200

    First Version

---
+++ RabbitMQ.Messaging.Receive/Receive.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Messaging
+{
+    class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("topic_render", "topic");
+                    channel.ExchangeDeclare("topic_import", "topic");
+                    channel.ConfirmSelect();
+
+                    var qRenderDaily =  channel.QueueDeclare("render.daily", true, false, false, null);
+                    //var qRenderNightly = channel.QueueDeclare("render.nightly", true, false, false, null);
+                    //var qImportDaily = channel.QueueDeclare("import.daily", true, false, false, null);
+
+                    channel.QueueBind(qRenderDaily, "topic_render", "daily");
+                    //channel.QueueBind(qRenderNightly, "topic_render", "nightly");
+                    //channel.QueueBind(qImportDaily, "topic_import", "daily");
+                   
+
+                    Console.WriteLine(" [*] Waiting for messages. " +
+                                      "To exit press CTRL+C");
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume(qRenderDaily, false, consumer);
+                    //channel.BasicConsume(qRenderNightly, true, consumer);
+                    //channel.BasicConsume(qImportDaily, true, consumer);
+
+                    while (true)
+                    {
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var routingKey = ea.RoutingKey;
+                        channel.BasicAck(ea.DeliveryTag, false);
+
+                        Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                          routingKey, message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ.Messaging/Send.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Messaging
+{
+    class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    for (int i = 0; i < 100000; i++)
+                    {
+                        string message = "Hello World!: " + i ;
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        var properties = channel.CreateBasicProperties();
+                        properties.SetPersistent(true);
+                        channel.ConfirmSelect();
+                        channel.BasicPublish("topic_render", "daily", properties, body);
+                        //.BasicPublish("topic_render", "nightly", properties, body);
+                        //channel.BasicPublish("topic_import", "daily", properties, body);
+
+
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }  
+                }
+            }
+        }
+    }
+}
